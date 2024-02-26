8a12f73aa4554f599b6c05209aa9d06257acdf4d
Harsh-Vaishani <harshvaishnani8@gmail.com>
2024-02-26 08:19:32 +0530

    5.2-lecture

---
+++ lec-5.2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//    int age;
+//    cout<<"Enter your age ";
+//    cin>>age;
+//    if(age>=18){
+//     cout<<"Yes"<<endl;
+//    }
+//    else if(age>18 || age>=0){
+//     cout<<"No"<<endl;
+//    }
+//    else{
+//     cout<<"Invalid Age";
+//    }
+// }
+
+
+// int main()
+// {
+//     int age;
+//     cout<<"Enter age ";
+//     cin>>age;
+//     (age >= 18) ?  cout << "Yes" : cout << "No";
+// }
+
+
+// int main(){
+//     int day;
+//     cout<<"enter day name ";
+//     cin>>day;
+//     switch (day){
+//         case 's':cout<<"sunday";
+//         break;
+//         case 'm':cout<<"monday";
+//         break;
+//         case 't' :cout<<"tuesday";
+//         break;
+//         default:cout<<"sorry";
+//     }
+// }
+
+// int main(){
+//     int i;
+//     int n;
+//     int sum;
+//     sum=0; 
+//     cout<<"enter any number ";
+//     cin>>n;
+//     for(i=0; i<=n; i++){
+//         sum=sum+i;
+//     }
+//     cout<<sum;
+// }
+
